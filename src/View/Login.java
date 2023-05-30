5e5fad8ab171424f837d7667582bbd36fda418cd
Carlos Gahel Pulido Cervantes <carlosgahelpulidocervantes@gmail.com>
2023-05-29 20:11:22 -0700

    Actualización de la pantalla de Login

--- src/View/Login.java
+++ src/View/Login.java
@@ -4,15 +4,18 @@ import javax.swing.*;
 
 public class Login extends JFrame {
     private JPanel contentPanel;
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JButton iniciarSesiónButton;
+    private JProgressBar progressBar1;
 
     public Login() {
         FlatDarkLaf.setup();
         setContentPane(contentPanel);
         setTitle("Inicio de sesión");
-        setSize(400, 300);
+        setSize(300, 170);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
-        pack();
     }
 }
