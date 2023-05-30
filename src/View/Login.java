cc11fb54f28826e77ee3d38947949ba7acbc4164
Carlos Gahel Pulido Cervantes <carlosgahelpulidocervantes@gmail.com>
2023-05-29 19:55:11 -0700

    Actualización de la pantalla de Login

--- src/View/Login.java
+++ src/View/Login.java
@@ -1,8 +1,18 @@
 package View;
-
+import com.formdev.flatlaf.FlatDarkLaf;
 import javax.swing.*;
 
 public class Login extends JFrame {
-
     private JPanel contentPanel;
+
+    public Login() {
+        FlatDarkLaf.setup();
+        setContentPane(contentPanel);
+        setTitle("Inicio de sesión");
+        setSize(400, 300);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        pack();
+    }
 }
--- src/main/Startup.java
+++ src/main/Startup.java
@@ -6,6 +6,5 @@ public class Startup extends javax.swing.JFrame {
     public static void main(String[] args) {
         FlatDarkLaf.setup();
         Login login = new Login();
-        login.setVisible(true);
     }
 }
