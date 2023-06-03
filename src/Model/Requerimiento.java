3a76e00e677d18970444ec5262d9b24b7b16f10b
Carlos Gahel Pulido Cervantes <carlosgahelpulidocervantes@gmail.com>
2023-06-03 15:00:01 -0700

    Creación del diagrama y realización de la clase de requerimientos (Incompleta)

--- src/Model/Requerimiento.java
+++ src/Model/Requerimiento.java
@@ -1,6 +1,7 @@
 package Model;
 
 import java.math.BigDecimal;
+import java.util.Calendar;
 import java.util.Date;
 import java.time.LocalDate;
 
@@ -125,7 +126,216 @@ public class Requerimiento {
         Notificacion();
     }
 
+    public String getDictaminador() {
+        return dictaminador;
+    }
+
+    public void setDictaminador(String dictaminador) {
+        this.dictaminador = dictaminador;
+    }
+
+    public long getNumeroDeControl() {
+        return numeroDeControl;
+    }
+
+    public void setNumeroDeControl(long numeroDeControl) {
+        this.numeroDeControl = numeroDeControl;
+    }
+
+    public String getRFC() {
+        return RFC;
+    }
+
+    public void setRFC(String RFC) {
+        this.RFC = RFC;
+    }
+
+    public String getNombreORazonSocial() {
+        return nombreORazonSocial;
+    }
+
+    public void setNombreORazonSocial(String nombreORazonSocial) {
+        this.nombreORazonSocial = nombreORazonSocial;
+    }
+
+    public String getTramite() {
+        return tramite;
+    }
+
+    public void setTramite(String tramite) {
+        this.tramite = tramite;
+    }
+
+    public String getTipoDeTramite() {
+        return tipoDeTramite;
+    }
+
+    public void setTipoDeTramite(String tipoDeTramite) {
+        this.tipoDeTramite = tipoDeTramite;
+    }
+
+    public Date getFechaDeRecepcion() {
+        return fechaDeRecepcion;
+    }
+
+    public void setFechaDeRecepcion(Date fechaDeRecepcion) {
+        this.fechaDeRecepcion = fechaDeRecepcion;
+    }
+
+    public Mes getPeriodo() {
+        return periodo;
+    }
+
+    public void setPeriodo(Mes periodo) {
+        this.periodo = periodo;
+    }
+
+    public int getEjercicio() {
+        return ejercicio;
+    }
+
+    public void setEjercicio(int ejercicio) {
+        this.ejercicio = ejercicio;
+    }
+
+    public EstadoDeTramite getEstadoDeTramite() {
+        return estadoDeTramite;
+    }
+
+    public void setEstadoDeTramite(EstadoDeTramite estadoDeTramite) {
+        this.estadoDeTramite = estadoDeTramite;
+    }
+
+    public Date getFechaDeResolucion() {
+        return fechaDeResolucion;
+    }
+
+    public void setFechaDeResolucion(Date fechaDeResolucion) {
+        this.fechaDeResolucion = fechaDeResolucion;
+    }
+
+    public BigDecimal getImporteSolicitado() {
+        return importeSolicitado;
+    }
+
+    public void setImporteSolicitado(BigDecimal importeSolicitado) {
+        this.importeSolicitado = importeSolicitado;
+    }
+
+    public BigDecimal getImporteAutorizado() {
+        return importeAutorizado;
+    }
+
+    public void setImporteAutorizado(BigDecimal importeAutorizado) {
+        this.importeAutorizado = importeAutorizado;
+    }
+
+    public BigDecimal getCompensacionPorOficio() {
+        return compensacionPorOficio;
+    }
+
+    public void setCompensacionPorOficio(BigDecimal compensacionPorOficio) {
+        this.compensacionPorOficio = compensacionPorOficio;
+    }
+
+    public Date getFechaDePago() {
+        return fechaDePago;
+    }
+
+    public void setFechaDePago(Date fechaDePago) {
+        this.fechaDePago = fechaDePago;
+    }
+
+    public BigDecimal getImportePagado() {
+        return importePagado;
+    }
+
+    public void setImportePagado(BigDecimal importePagado) {
+        this.importePagado = importePagado;
+    }
+
+    public Date getFechaDeRequerimiento() {
+        return fechaDeRequerimiento;
+    }
+
+    public void setFechaDeRequerimiento(Date fechaDeRequerimiento) {
+        this.fechaDeRequerimiento = fechaDeRequerimiento;
+    }
+
+    public Date getFechaDeSegundoRequerimiento() {
+        return fechaDeSegundoRequerimiento;
+    }
+
+    public void setFechaDeSegundoRequerimiento(Date fechaDeSegundoRequerimiento) {
+        this.fechaDeSegundoRequerimiento = fechaDeSegundoRequerimiento;
+    }
+
+    public Prioridad getPrioridad() {
+        return prioridad;
+    }
+
+    public void setPrioridad(Prioridad prioridad) {
+        this.prioridad = prioridad;
+    }
+
     public Requerimiento Notificacion() {
         return this;
+    }
+
+    public void DeterminarPrioridad() {
+        // Comparar la cantidad de días desde la fecha de creacion del requerimiento
+        if (obtenerDiferenciaDias(this.fechaDeRecepcion) > 29) {
+            this.prioridad = Prioridad.ALTA;
+        } else {
+            this.prioridad = Prioridad.BAJA;
+        }
+    }
+
+    public static int obtenerDiferenciaDias(Date fecha) {
+        // Obtiene la fecha actual
+        Date fechaActual = new Date();
+
+        // Crea los objetos de calendario para las fechas
+        Calendar calFecha = Calendar.getInstance();
+        calFecha.setTime(fecha);
+
+        Calendar calFechaActual = Calendar.getInstance();
+        calFechaActual.setTime(fechaActual);
+
+        // Descarta las horas, minutos, segundos y milisegundos de los calendarios
+        calFecha.set(Calendar.HOUR_OF_DAY, 0);
+        calFecha.set(Calendar.MINUTE, 0);
+        calFecha.set(Calendar.SECOND, 0);
+        calFecha.set(Calendar.MILLISECOND, 0);
+
+        calFechaActual.set(Calendar.HOUR_OF_DAY, 0);
+        calFechaActual.set(Calendar.MINUTE, 0);
+        calFechaActual.set(Calendar.SECOND, 0);
+        calFechaActual.set(Calendar.MILLISECOND, 0);
+
+        // Calcula la diferencia en milisegundos
+        long diferenciaMilisegundos = calFechaActual.getTimeInMillis() - calFecha.getTimeInMillis();
+
+        // Calcula la diferencia en días sin contar sábados ni domingos
+        int diferenciaDias = 0;
+        int diasNoContados = 0;
+
+        while (calFecha.before(calFechaActual)) {
+            calFecha.add(Calendar.DAY_OF_MONTH, 1);
+
+            if (calFecha.get(Calendar.DAY_OF_WEEK) != Calendar.SATURDAY &&
+                    calFecha.get(Calendar.DAY_OF_WEEK) != Calendar.SUNDAY) {
+                diferenciaDias++;
+            } else {
+                diasNoContados++;
+            }
+        }
+
+        // Resta los días no contados si hay más de 5 días de diferencia
+        if (diasNoContados > 5) {
+            diferenciaDias -= (diasNoContados - 5);
+        }
+
+        return diferenciaDias;
     }
 }
